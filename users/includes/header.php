f02658d32451fed010458025f53ffb3204637f6a
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-08-05 10:29:55 -0700

    Moved jquery script

--- users/includes/header.php
+++ users/includes/header.php
@@ -157,19 +157,20 @@ include $abs_us_root.$us_url_root."users/lang/en-US.php";
 	  integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg="
 	  crossorigin="anonymous"></script> -->
 
-	<script
+	<!-- <script
 	  src="https://code.jquery.com/jquery-3.4.1.js"
 	  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-	  crossorigin="anonymous"></script>
+	  crossorigin="anonymous"></script> -->
 
-	<!-- jQuery Fallback -->
-	<!-- <script type="text/javascript">
-	if (typeof jQuery == 'undefined') {
-		document.write(unescape("%3Cscript src='<?=$us_url_root?>users/js/jquery3_4_1.js' type='text/javascript'%3E%3C/script%3E"));
-	}
-	</script> -->
+
 
 	<?php require_once $abs_us_root.$us_url_root.'usersc/includes/bootstrap_corrections.php'; ?>
+
+
+<script
+	src="https://code.jquery.com/jquery-3.4.1.js"
+	integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+	crossorigin="anonymous"></script>
 
 <script src="<?=$us_url_root?>users/js/tomfoolery.js"></script>
 <?php if(!isset($_SESSION['fingerprint'])) {?>
