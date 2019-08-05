dbb161b702f920be5997b2adfb7445e819414aeb
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-08-05 09:55:55 -0700

    Added jquery 3.4.1

--- users/includes/header.php
+++ users/includes/header.php
@@ -157,9 +157,9 @@ include $abs_us_root.$us_url_root."users/lang/en-US.php";
 	  integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg="
 	  crossorigin="anonymous"></script> -->
 
-		<script
-	  src="https://code.jquery.com/jquery-3.0.0.js"
-	  integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo="
+	<script
+	  src="https://code.jquery.com/jquery-3.4.1.js"
+	  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
 	  crossorigin="anonymous"></script>
 
 	<!-- jQuery Fallback -->
