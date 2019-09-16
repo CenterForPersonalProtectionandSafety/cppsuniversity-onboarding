b02a56c4c3df2b1fb5e72c62684e81e571135ff4
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-09-16 15:04:05 -0700

    Fixed PHPMailer path

--- usersc/email_test.php
+++ usersc/email_test.php
@@ -13,9 +13,9 @@ if (!securePage($_SERVER['PHP_SELF'])){die();}
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '/PHPMailer/src/Exception.php';
-require '/PHPMailer/src/PHPMailer.php';
-require '/PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 
 if(isset($_POST["Email"])){
