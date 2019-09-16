6c5613fa56a8c68a3c9757cd24893d6a9b7ed1dd
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-09-16 15:05:49 -0700

    updated test email settings

--- usersc/email_test.php
+++ usersc/email_test.php
@@ -53,11 +53,11 @@ if(isset($_POST["Email"])){
   $mail->Subject = 'PHPMailer GMail SMTP test';
   //Read an HTML message body from an external file, convert referenced images to embedded,
   //convert HTML into a basic plain-text alternative body
-  $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+  //$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
   //Replace the plain text body with one created manually
   $mail->AltBody = 'This is a plain-text message body';
   //Attach an image file
-  $mail->addAttachment('images/phpmailer_mini.png');
+  //$mail->addAttachment('images/phpmailer_mini.png');
   //send the message, check for errors
   if (!$mail->send()) {
       echo "Mailer Error: " . $mail->ErrorInfo;
